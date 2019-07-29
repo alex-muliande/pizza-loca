bcf4b6802d8ec7f603724250fc93c06b26fb21f3
alex-muliande <amuliande@gmail.com>
2019-07-29 15:20:39 +0300

    Add java script content to input out the price of the pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,54 @@
-$("input[name='status']").on("click", function() {
-  $(".address").toggle(this.value == "200" && this.checked);
- });
 //business logic
-
-function quantity() {
-   var selectedQuantity = document.getElementById("quantity").value;
-       console.log ("selectedQuantity");
+function Details(type, size, crust, toppings, quantity){
+  this.selectedType = type;
+  this.selectedSize = size;
+  this.selectedCrust = crust;
+  this.selectedToppings = toppings;
+  this.selectedQuantity = quantity;
 }
-  console.log(selectedQuantity);
-function getSizeValue() {  var selectedValue = document.getElementById("pizzaSize").value;  return parseInt(selectedValue);}
 
+function Total(price, quantity, delivery){
+  this.price = price;
+  this.quantity = quantity;
+  this.delivery = delivery;
+}
 
+Total.prototype.finalTotal = function(){
+  return this.price * this.quantity + this.delivery;
+}
 
 
 
+  //ui logic
+$(document).ready(function(){
+  $('#form2').submit(function(event){
+    event.preventDefault();
 
+    var selectedDeliver = parseInt($('#delivery-option').val());
+    var deliverPrices = [200, 0]
+    var sizePrices = [600, 900, 1200];
+    var inputtedType = $('#type').val();
+    var inputtedCrust = $('#crust').val();
+    var inputtedTopping = $('#toppings').val();
+    var inputtedSize = parseInt($('#size').val());
+    var pizzaPrice = sizePrices[inputtedSize-1];
+    var inputtedQuantity = parseInt($('#quantity').val());
+    var deliveryPrice = deliverPrices[selectedDeliver-1];
 
+    var newDetails = new Details(inputtedType, inputtedSize, inputtedCrust, inputtedTopping, inputtedQuantity);
+    var newTotal = new Total(pizzaPrice, inputtedQuantity, deliveryPrice);
+    var newBill = newTotal.finalTotal();
 
 
+    alert("your order is " + newDetails.selectedType + ", " + newDetails.selectedToppings + ', and ' + newDetails.selectedCrust +". click ok to view your bill");
+    alert(newBill +"");
 
+    document.getElementById('myOrder').reset();
 
+  });
 
 
+});
 
 
 
@@ -90,28 +117,6 @@ function getSizeValue() {  var selectedValue = document.getElementById("pizzaSiz
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     function contact(form){
-      var name = document.forms["form2"]["name"].value;
-          var email = document.forms["form2"]["email"].value;
-          var message = document.forms["form2"]["message"].value;
-          alert("Hello! " + name + "," + " We have received your message. ");
-            };
 
 
           //   $("input[name='delivery']").on("click", function(){
